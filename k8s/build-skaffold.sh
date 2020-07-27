7d75ccfd90f57740772e3943be6d1e9fb3c71528
Jorge Bautista <jorgebautista@gmail.com>
2020-07-26 23:59:27 -0400

    fixed image for skaffold in dockerfile

--- k8s/build-skaffold.sh
+++ k8s/build-skaffold.sh
@@ -2,7 +2,7 @@
 #BUILD OUR APP
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -ldflags="-w -s" -o ./makako-api
 #BUILD THE IMAGE
-docker build --build-arg DOCKER_GIT_CREDENTIALS -t registry.gitlab.com/jebo87/makako-api:test -f `pwd`/k8s/Dockerfile .
+docker build -t registry.gitlab.com/jebo87/makako-api:test -f `pwd`/k8s/Dockerfile .
 #PUSH TO OUR REGISTRY
 docker push registry.gitlab.com/jebo87/makako-api:test
 #REMOVE UNUSED IMAGES
